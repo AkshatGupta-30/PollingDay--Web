646fc8608c02b8d04b3adc0e609541134dc8647d
AkshatGupta-30 <akshatgupta0030@gmail.com>
2024-05-23 07:30:51 +0530

    refactor: Functional component to class component

--- client/src/common/Navbar.js
+++ client/src/common/Navbar.js
@@ -1,36 +1,64 @@
-import React from "react";
-import '../css/Navbar.css';
+import React, { Component } from "react";
+import "../css/Navbar.css";
 
-function Navbar() {
-  return (<div id="header">
-    <input type="checkbox" id="menuToggle" />
-      <div className="navbar">
-        <div className="logo">
-          <div className="toggleMenu">
-            <i className="bi bi-list"></i>
+class Navbar extends Component {
+  render() {
+    return (
+      <div id="header">
+        <input type="checkbox" id="menuToggle" />
+        <div className="navbar">
+          <div className="logo">
+            <div className="toggleMenu">
+              <i className="bi bi-list"></i>
+            </div>
+            <a href=".">Polling Day</a>
           </div>
-          <a href=".">Polling Day</a>
+          <ul className="links">
+            <li>
+              <a href="." className="inactive">
+                Parties
+              </a>
+            </li>
+            <li>
+              <a href="." className="inactive">
+                Candidates
+              </a>
+            </li>
+            {/* //TODO : Add Links to Items */}
+            <li>
+              <a href="." className="inactive">
+                Voting
+              </a>
+            </li>
+            <li>
+              <a href="." className="inactive">
+                Results
+              </a>
+            </li>
+          </ul>
         </div>
-        <ul className="links">
-          <li><a href="." className="inactive">Parties</a></li>
-          <li><a href="." className="inactive">Candidates</a></li>
-          {/* //TODO : Add Links to Items */}
-          <li><a href="." className="inactive">Voting</a></li>
-          <li><a href="." className="inactive">Results</a></li>
-        </ul>
+        <div className="side-nav">
+          <ul className="links">
+            {/* //TODO: Side navigation drop down on links */}
+            <a href="." className="inactive">
+              Parties
+            </a>
+            {/* //TODO: Side navigation drop down on links */}
+            <a href="." className="inactive">
+              Candidates
+            </a>
+            {/* //TODO : Add Links to Items */}
+            <a href="." className="inactive">
+              Voting
+            </a>
+            <a href="." className="inactive">
+              Results
+            </a>
+          </ul>
+        </div>
       </div>
-      <div className="side-nav">
-        <ul className="links">
-          {/* //TODO: Side navigation drop down on links */}
-          <a href="." className="inactive">Parties</a>
-          {/* //TODO: Side navigation drop down on links */}
-          <a href="." className="inactive">Candidates</a>
-          {/* //TODO : Add Links to Items */}
-          <a href="." className="inactive">Voting</a>
-          <a href="." className="inactive">Results</a>
-        </ul>
-      </div>
-  </div>);
+    );
+  }
 }
 
 export default Navbar;
