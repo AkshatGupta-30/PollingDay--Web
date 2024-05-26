55383d126b88370022eefecff597a57184515177
AkshatGupta-30 <akshatgupta0030@gmail.com>
2024-05-26 11:12:28 +0530

    feat: Used usestate to get form data

--- client/src/pages/PartyRegistration/PartyRegistration.jsx
+++ client/src/pages/PartyRegistration/PartyRegistration.jsx
@@ -1,104 +1,131 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import "./PartyRegistration.scss";
 import Navbar from "../../components/Navbar/Navbar";
 import Footer from "../../components/Footer/Footer";
 
 const PartyRegistration = () => {
+  const [party, setParty] = useState({
+    name: "", leader: "", leaderMobile: "", foundedYear: "", ideology: "", headquarter: "", numberOfMembers: ""
+  });
+
+  let name, value;
+
+  const changedValues = (ev) => {
+    console.log(ev.target.name + "  :  " + ev.target.value)
+    name = ev.target.name;
+    value = ev.target.value;
+    setParty({...party, [name]:value})
+  };
+
   return (
     <div className="PartyRegistrationPage">
       <Navbar active="0" />
       <main>
-        <div class="heading">
-          <Link class="material-symbols-outlined backBtn" to={"/parties"}>
+        <div className="heading">
+          <Link className="material-symbols-outlined backBtn" to={"/parties"}>
             arrow_back
           </Link>
-          <label for="page_heading">Party Registration</label>
+          <label htmlFor="page_heading">Party Registration</label>
         </div>
-        <div class="formSection">
+        <div className="formSection">
           <form>
-            <div class="input-container">
+            <div className="input-container">
               <input
                 type="text"
-                id="party_name"
-                name="party_name"
+                name="name"
                 placeholder="Party Name"
+                autoComplete="off"
+                value={party.name}
+                onChange={changedValues}
                 required
               />
-              <div class="bi bi-tags-fill icon"></div>
+              <div className="bi bi-tags-fill icon"></div>
             </div>
 
-            <div class="input-container">
+            <div className="input-container">
               <input
                 type="text"
-                id="leader_name"
-                name="leader_name"
+                name="leader"
                 placeholder="Leader Name"
+                autoComplete="off"
+                value={party.leader}
+                onChange={changedValues}
                 required
               />
-              <div class="bi bi-person-fill icon"></div>
+              <div className="bi bi-person-fill icon"></div>
             </div>
 
 
-            <div class="input-container">
+            <div className="input-container">
               <input
-                id="leader_mobile"
-                name="leader_mobile"
+                name="leaderMobile"
                 placeholder="Leader Mobile Number"
+                autoComplete="off"
+                value={party.leaderMobile}
+                onChange={changedValues}
                 inputMode="numeric"
                 minLength={10} maxLength={10}
                 required
               />
-              <div class="bi bi-telephone-fill icon"></div>
+              <div className="bi bi-telephone-fill icon"></div>
             </div>
 
-            <div class="input-container">
+            <div className="input-container">
               <input
                 type="number"
-                id="founded_year"
-                name="founded_year"
+                name="foundedear"
                 placeholder="Founded Year"
-                min="1995" value="1995" max={new Date().getFullYear()}
+                autoComplete="off"
+                value={party.foundedYear}
+                onChange={changedValues}
+                min="1995" max={new Date().getFullYear()}
                 required
               />
-              <div class="bi bi-calendar-check-fill icon"></div>
+              <div className="bi bi-calendar-check-fill icon"></div>
             </div>
 
-            <div class="input-container">
+            <div className="input-container">
               <input
                 type="text"
-                id="ideology"
                 name="ideology"
                 placeholder="Idealogy"
+                autoComplete="off"
+                value={party.ideology}
+                onChange={changedValues}
                 required
               />
-              <div class="bi bi-lightbulb-fill icon"></div>
+              <div className="bi bi-lightbulb-fill icon"></div>
             </div>
 
-            <div class="input-container">
+            <div className="input-container">
               <input
                 type="text"
-                id="hq_location"
-                name="hq_location"
+                name="headquarter"
                 placeholder="Headquarter Location"
+                autoComplete="off"
+                value={party.headquarter}
+                onChange={changedValues}
                 required
               />
-              <div class="bi bi-geo-fill icon"></div>
+              <div className="bi bi-geo-fill icon"></div>
             </div>
 
-            <div class="input-container">
+            <div className="input-container">
               <input
                 type="number"
-                id="number_of_members"
-                name="number_of_members"
+                name="numberOfMembers"
                 placeholder="Number of members"
+                autoComplete="off"
+                value={party.numberOfMembers}
+                onChange={changedValues}
                 min="10"
                 required
               />
-              <div class="bi bi-people icon"></div>
+              <div className="bi bi-people icon"></div>
             </div>
 
-            <div class="formButton">
+            <div className="formButton">
               <button type="reset" id="clear_all_btn">
                 Clear All
               </button>
@@ -107,7 +134,7 @@ const PartyRegistration = () => {
               </button>
             </div>
           </form>
-          <div class="templateImage">
+          <div className="templateImage">
             <img
               src={require("../../assets/images/party_registration_side_image.jpg")}
               alt="Party"
